5671e4e3a589985f49cd10adfb905d6a1cf390c0
Nituv05 <Tinbgbg05@gmail.com>
2025-05-03 23:30:29 +0700

    Update frontend

---
+++ webML/scripts.js
@@ -0,0 +1,22 @@
+function goToLandingPage() {
+    window.location.href = '/static/landingpage.html';
+}
+
+function goToLogin() {
+    window.location.href = '/static/login.html';
+}
+
+function goToRegister() {
+    window.location.href = '/static/register.html';
+}
+
+function goToDashboard() {
+    window.location.href = '/static/dashboard.html';
+}
+
+
+
+// Hàm đăng xuất (quay lại landing page)
+function logout() {
+    window.location.href = '/static/landingpage.html';
+}
